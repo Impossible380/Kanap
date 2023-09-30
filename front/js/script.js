bc79e317a8d69b9dff755302686dba060c766ea4
Impossible380 <impossible380@tutanota.com>
2023-09-30 09:17:27 +0200

    premier script

---
+++ front/js/script.js
@@ -0,0 +1,16 @@
+// requêter l'API pour lui demander l'ensemble des produits
+// parcourir les produits
+// insérer chaque produit dans le DOM de la page d'accueil
+
+           
+
+const html_produit = `
+<a href="./product.html?id=42">
+    <article>
+        <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
+        <h3 class="productName">Kanap name1</h3>
+        <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
+    </article>
+</a>`
+
+console.log(html_produit)
