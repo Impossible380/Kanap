7c696e2ec67973336d7f2254564a45b81c73a7e2
Impossible380 <impossible380@tutanota.com>
2023-10-02 18:01:35 +0200

    integrations des produits

--- front/js/script.js
+++ front/js/script.js
@@ -1,16 +1,34 @@
-// requêter l'API pour lui demander l'ensemble des produits
-// parcourir les produits
-// insérer chaque produit dans le DOM de la page d'accueil
+// 1 requêter l'API pour lui demander l'ensemble des produits
+// 2 parcourir les produits
+// 3 insérer chaque produit dans le DOM de la page d'accueil
 
-           
+const resultats = await fetch("http://localhost:3000/api/products");
+const json = await resultats.json();
 
-const html_produit = `
-<a href="./product.html?id=42">
-    <article>
-        <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
-        <h3 class="productName">Kanap name1</h3>
-        <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
-    </article>
-</a>`
+for (let i = 0 ; i < json.length ; i++) {
+    const id = json[i]["_id"];
+    const image_description = json[i]["altTxt"];
+    const colors = json[i]["colors"];
+    const product_description = json[i]["description"];
+    const image = json[i]["imageUrl"];
+    const name = json[i]["name"];
+    const price = json[i]["price"];
 
-console.log(html_produit)
+    const html_produit = `
+    <a href="./product.html?id=${id}">
+        <article>
+            <img src="${image}" alt="${image_description}">
+            <h3 class="productName">${name}</h3>
+            <p class="productDescription">${product_description}</p>
+            <p class="productColors"><b>Couleurs :</b> ${colors}</p>
+            <p class="productPrice"><b>Prix :</b> ${price} €</p>
+        </article>
+    </a>`;
+
+    document.getElementById("items").innerHTML += html_produit;
+
+    // console.log(html_produit);
+};
+
+console.log(json);
+// console.log(json[0]["_id"]);
