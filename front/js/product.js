e54faee6387e23169b7f3d6cbce9cd17d72c05ec
Impossible380 <impossible380@tutanota.com>
2023-10-05 19:06:25 +0200

    product_js allege

--- front/js/product.js
+++ front/js/product.js
@@ -1,30 +1,42 @@
-/* const paramsString = "q=URLUtils.searchParams&topic=api";
-const searchParams = new URLSearchParams(paramsString);
+/* array.forEach(callback) : parcourir les éléments
+array.map(callback) : transformer le tableau en changeant la structure des éléments
+array.find(callback) : trouver un des éléments du tableau
+array.filter(callback) : récupérer un tableau avec moins d'éléments (filtrer)
+array.join(séparateur) : concaténer tous les éléments dans une seule chaîne de caractères */
 
-console.log(searchParams.get("topic"));
-console.log(searchParams.get("q")); */
+
+// Retourne un tableau de tous les éléments
+http://localhost:3000/api/products/
+
+// Renvoie l'élément correspondant à
+// {product-ID}, identifiant d’un produit
+http://localhost:3000/api/products/{product_id}
+
+// Retourne l'objet contact, le tableau
+// produits et orderId
+http://localhost:3000/api/products/order
+
 
 const current_url = document.location.search;
 const searchParams_product = new URLSearchParams(current_url);
-console.log(searchParams_product.get("id"));
-
-// document.querySelector(".item").innerHTML += searchParams_product.get("id");
+const id = searchParams_product.get("id")
 
 const response = await fetch("http://localhost:3000/api/products");
 const products = await response.json();
 
-for (let i = 0 ; i < products.length ; i++) {
-    const product = products[i];
-    console.log(product._id == searchParams_product.get("id"));
-    if (product._id == searchParams_product.get("id")) {
-        document.querySelector(".item__img").innerHTML = `
-        <img src="${product.imageUrl}" alt="${product.altTxt}">`;
-        document.querySelector("#title").innerHTML = product.name;
-        document.querySelector("#price").innerHTML = product.price;
-        document.querySelector("#description").innerHTML = product.description;
-        for (let j = 0 ; j < product.colors.length ; j++) {
-            document.querySelector("#colors").innerHTML += `
-            <option value="${product.colors[j]}">${product.colors[j]}</option>`;
-        }
-    }
-}
+const product = products
+    .find(function (p) {
+        return p._id == id
+    });
+
+document.querySelector(".item__img").innerHTML = `
+<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+document.querySelector("#title").innerHTML = product.name;
+document.querySelector("#price").innerHTML = product.price;
+document.querySelector("#description").innerHTML = product.description;
+
+document.querySelector("#colors").innerHTML += product.colors
+    .map(function (color) {
+        return `<option value="${color}">${color}</option>`
+    })
+    .join("")
