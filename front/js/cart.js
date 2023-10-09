bf146f3f2c9411e0670a4f83385034c8cbc1a756
Impossible380 <impossible380@tutanota.com>
2023-10-09 18:58:03 +0200

    affichage panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,21 +1,29 @@
-const cart = localStorage.cart;
-const item = cart.item;
+const cart = JSON.parse(localStorage.cart);
 
-for (let i = 0 ; i < cart.length ; i++) {document.querySelector("#cart__items").innerHTML = `
+console.log(cart);
+
+for (let i = 0 ; i < cart.length ; i++) {
+    const item = cart[i];
+    console.log(item);
+
+    const response = await fetch(`http://localhost:3000/api/products/${item.id}`);
+    const product = await response.json();
+    
+    document.querySelector("#cart__items").innerHTML += `
     <article class="cart__item" data-id="${item.id}" data-color="${item.color}">
         <div class="cart__item__img">
-            <img src="" alt="">
+            <img src="${product.imageUrl}" alt="${product.altTxt}">
         </div>
         <div class="cart__item__content">
             <div class="cart__item__content__description">
-                <h2></h2>
-                <p>Vert</p>
-                <p>42,00 €</p>
+                <h2>${product.name}</h2>
+                <p>${item.color}</p>
+                <p>${product.price} €</p>
             </div>
             <div class="cart__item__content__settings">
                 <div class="cart__item__content__settings__quantity">
                     <p>Qté : </p>
-                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantity}">
                 </div>
                 <div class="cart__item__content__settings__delete">
                     <p class="deleteItem">Supprimer</p>
@@ -27,35 +35,17 @@ for (let i = 0 ; i < cart.length ; i++) {document.querySelector("#cart__items").
 
 
 
-/* const current_url = document.location.search;
-const searchParams_product = new URLSearchParams(current_url);
-const id = searchParams_product.get("id");
 
-const response = await fetch(`http://localhost:3000/api/products/${id}`);
-const product = await response.json();
+/* Problèmes
+
+1) Si le panier est vide, ça va planter sur cette page.
+
+*/
 
 
-console.log(localStorage.cart);
+/* A faire
 
-document.querySelector("#cart__items").innerHTML = `
-<article class="cart__item" data-id="${product.id}" data-color="${product.color}">
-    <div class="cart__item__img">
-        <img src="${product.image}" alt="${product.altTxt}">
-    </div>
-    <div class="cart__item__content">
-        <div class="cart__item__content__description">
-            <h2>${product.name}</h2>
-            <p>Vert</p>
-            <p>42,00 €</p>
-        </div>
-        <div class="cart__item__content__settings">
-            <div class="cart__item__content__settings__quantity">
-                <p>Qté : </p>
-                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-            </div>
-            <div class="cart__item__content__settings__delete">
-                <p class="deleteItem">Supprimer</p>
-            </div>
-        </div>
-    </div>
-</article>`; */
+1) Gérer la suppresion
+2) Gérer la modification
+
+*/
