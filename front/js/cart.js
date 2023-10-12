b7768594d833a765326a8d6f2b5abe96c266d722
Impossible380 <impossible380@tutanota.com>
2023-10-12 18:12:58 +0200

    cart variable article

--- front/js/cart.js
+++ front/js/cart.js
@@ -23,42 +23,8 @@ const cart = JSON.parse(localStorage.cart);
 console.log(localStorage.cart);
 console.log(cart);
 
-function affichageTempsReel() {
-    let total_quantity = 0;
-    let total_price = 0;
-    
-    for (let i = 0 ; i < cart.length ; i++) {
-        document.getElementById("cart__items").innerHTML += `
-        <article class="cart__item" data-id="${item.id}" data-color="${item.color}">
-            <div class="cart__item__img">
-                <img src="${product.imageUrl}" alt="${product.altTxt}">
-            </div>
-            <div class="cart__item__content">
-                <div class="cart__item__content__description">
-                    <h2>${product.name}</h2>
-                    <p>${item.color}</p>
-                    <p>${product.price} €</p>
-                </div>
-                <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                        <p>Qté : </p>
-                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantity}">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                        <p class="deleteItem">Supprimer</p>
-                    </div>
-                </div>
-            </div>
-        </article>`;
-    
-        total_quantity += item.quantity;
-        total_price += item.quantity * product.price;
-    };
-    
-    document.getElementById("totalQuantity").innerHTML = total_quantity;
-    document.getElementById("totalPrice").innerHTML = total_price;
-};
 
+let articles_list = []
 
 let total_quantity = 0;
 let total_price = 0;
@@ -73,7 +39,7 @@ for (let i = 0 ; i < cart.length ; i++) {
     const response = await fetch(`http://localhost:3000/api/products/${item.id}`);
     const product = await response.json();
 
-    document.getElementById("cart__items").innerHTML += `
+    articles_list.push(`
     <article class="cart__item" data-id="${item.id}" data-color="${item.color}">
         <div class="cart__item__img">
             <img src="${product.imageUrl}" alt="${product.altTxt}">
@@ -94,77 +60,65 @@ for (let i = 0 ; i < cart.length ; i++) {
                 </div>
             </div>
         </div>
-    </article>`;
+    </article>`);
 
     total_quantity += item.quantity;
     total_price += item.quantity * product.price;
 };
 
+let articles_join = articles_list;
+articles_join.join();
+console.log(articles_join);
+document.getElementById("cart__items").innerHTML = articles_join;
+
 document.getElementById("totalQuantity").innerHTML = total_quantity;
 document.getElementById("totalPrice").innerHTML = total_price;
 
-// affichageTempsReel;
+
+const changeQuantity = document.querySelectorAll(".itemQuantity");
+console.log(changeQuantity);
 
 
-const deleteOfCart = document.querySelectorAll("p.deleteItem");
+const deleteOfCart = document.querySelectorAll(".deleteItem");
 console.log(deleteOfCart);
 
 for (let i = 0 ; i < cart.length ; i++) {
     /* console.log(cart);
     console.log(localStorage.cart); */
     
-    const item = cart[i];
-    console.log(item);
+    // const item = cart[i];
+    console.log(cart[i]);
+
+
+    changeQuantity[i].addEventListener('change', function() {
+        cart[i].quantity = parseInt(this.value);
+        console.log(cart[i]);
+        localStorage.cart = JSON.stringify(cart);
+    });
 
 
     const deleteOfCartElement = deleteOfCart[i];
     /* console.log(deleteOfCart);
     console.log(deleteOfCartElement); */
 
-    deleteOfCartElement.addEventListener('click', function() {
-        document.getElementById("cart__items").innerHTML = ``;
-        
-        const article = deleteOfCartElement.closest("article.cart__item");
-        console.log(article);
+    const article = deleteOfCartElement.closest("article.cart__item");
+    console.log(article);
 
+    article.addEventListener('click', function() {
+        console.log(article);
+    
         cart.splice(i, 1);
         console.log(cart);
         localStorage.cart = JSON.stringify(cart);
         console.log(localStorage.cart);
-
-        let total_quantity = 0;
-        let total_price = 0;
         
-        for (let i = 0 ; i < cart.length ; i++) {
-            document.getElementById("cart__items").innerHTML += `
-            <article class="cart__item" data-id="${item.id}" data-color="${item.color}">
-                <div class="cart__item__img">
-                    <img src="${product.imageUrl}" alt="${product.altTxt}">
-                </div>
-                <div class="cart__item__content">
-                    <div class="cart__item__content__description">
-                        <h2>${product.name}</h2>
-                        <p>${item.color}</p>
-                        <p>${product.price} €</p>
-                    </div>
-                    <div class="cart__item__content__settings">
-                        <div class="cart__item__content__settings__quantity">
-                            <p>Qté : </p>
-                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantity}">
-                        </div>
-                        <div class="cart__item__content__settings__delete">
-                            <p class="deleteItem">Supprimer</p>
-                        </div>
-                    </div>
-                </div>
-            </article>`;
-        
-            total_quantity += item.quantity;
-            total_price += item.quantity * product.price;
-        };
-        
-        document.getElementById("totalQuantity").innerHTML = total_quantity;
-        document.getElementById("totalPrice").innerHTML = total_price;
+        console.log(articles_list);
+        articles_list.splice(i, 1);
+        console.log(articles_list);
+        articles_join = articles_list;
+        articles_join.join();
+        console.log(articles_join);
+        document.getElementById("cart__items").innerHTML = articles_join;
     });
 };
 
