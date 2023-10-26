5fc97dd7b4f0ae9963efad3dea18b74607541640
Impossible380 <impossible380@tutanota.com>
2023-10-26 18:37:24 +0200

    resultat final

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -3,9 +3,3 @@ const searchParams_product = new URLSearchParams(current_url);
 const id = searchParams_product.get("orderId");
 
 document.getElementById("orderId").innerHTML += `${id}`;
-
-
-
-
-/* Vider le panier après validation de la commande
-Afficher un message après l'ajout d'un élément au panier */
